71e62b173a249debc16da7bf192bcc18a4166ae1
lomousmane <lomousmane@gmail.com>
2024-08-21 16:33:59 +0000

    summary gss_cat

---
+++ main.R
@@ -0,0 +1,22 @@
+library(tidyverse)
+library(here)
+library(gt)
+library(prettyR)
+# visualiser les 1er lignes
+gss_cat %>%
+  head(10) %>% 
+  gt() %>% 
+  gt::cols_align(align="center")
+# structure et dimension
+str(gss_cat)
+dim(gss_cat)
+# summary
+describe(gss_cat)
+# summarise the tvhours by marital
+tab<-gss_cat %>% 
+  group_by(marital) %>% 
+  summarise(mean_tv_hours=mean(tvhours,na.rm = T))
+# enregister le tableau
+write.csv(tab,here("mean_hours_by_marital.csv"))
+  
+
